b520516ac10856dbd10f35142f4b48eab14a10e7
Jamie <jamie.a.otoole@gmail.com>
2024-09-30 16:33:28 -0400

    oop

--- lib/navigation/router.dart
+++ lib/navigation/router.dart
@@ -1,6 +1,7 @@
 import 'package:bloc_list_tab_carousel/cubits/app_cubit.dart';
 import 'package:bloc_list_tab_carousel/pages/children/stateless_child_page.dart';
 import 'package:bloc_list_tab_carousel/pages/children/stateful_child_page.dart';
+import 'package:bloc_list_tab_carousel/pages/purchase_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -18,6 +19,13 @@ Route<dynamic> generateRoute(RouteSettings settings, BuildContext context) {
         builder: (context) => BlocProvider.value(
           value: BlocProvider.of<AppCubit>(context),
           child: const StatefulChildWidget(),
+        ),
+      );
+    case '/purchase':
+      return MaterialPageRoute(
+        builder: (context) => BlocProvider.value(
+          value: BlocProvider.of<AppCubit>(context),
+          child: const PurchasePage(),
         ),
       );
     default:
